165d3390c665cde9dfebd0422440c8a783793911
Taein142 <xodlsdldy@naver.com>
2024-02-01 17:25:10 +0900

    9회차

---
+++ ch09_multiFor/ch01_반복문_다중for문.py
@@ -0,0 +1,20 @@
+# 반복문 다중for문
+for j in range(1, 4):
+    for i in range(1, 11):
+        print(i, "번")
+
+# for i in range(1, 4):
+#     for j in range(1,11):
+#         print(i, j)
+#     print()
+
+# for i in range(3, 0, -1):
+#     for j in range(1,11):
+#         print(i, j)
+#     print()
+
+# for i in range(2, 10):
+#     print(i, "단")
+#     for j in range(1,10):
+#         print(f"{i} * {j} = {i*j}")
+#     print()

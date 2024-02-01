116afc3b6e0a86fb7d19032d3df24bee03a2642f
Taein142 <xodlsdldy@naver.com>
2024-02-01 17:15:30 +0900

    파이썬기초 깃허브 및 레파지 초기화 후 다시 함

---
+++ ch01_variables/ch01_변수.py
@@ -0,0 +1,31 @@
+# 변수에 값 저장
+age = 21
+name = "홍길동"
+print(age)
+print(name)
+
+# 값 변경
+age = 50
+name = "김길동"
+print(age)
+print(name)
+
+# # 값 복사
+age1 = age
+print(age1)
+
+# 새로운 값저장 방법
+a = 5
+b = 6
+print(a)
+print(b)
+a, b = 7, 8
+print(a)
+print(b)
+
+# 두개의 변수 값 바꾸기
+a,b = b,a
+print(a)
+print(b)
+
+
